69b0a28e775cd48046c07ec36b7edd71f7ad8980
Dimitar Kirilov <jikorax@gmail.com>
2023-04-03 00:55:23 -0700

    added user input

--- pages/1_📊_Recommendation.py
+++ pages/1_📊_Recommendation.py
@@ -1,93 +1,43 @@
 
 
 import streamlit as st
-import inspect
-import textwrap
 import pandas as pd
-import pydeck as pdk
-from utils import show_code
-
-
+import altair as alt
 from urllib.error import URLError
 
 
-def mapping_demo():
+def run_model(selected):
+    st.write("Running recommendation model with interests:")
+    for item in selected:
+        st.write(item)
+
+
+def recommendation():
     @st.cache_data
-    def from_data_file(filename):
-        url = (
-            "http://raw.githubusercontent.com/streamlit/"
-            "example-data/master/hello/v1/%s" % filename
-        )
-        return pd.read_json(url)
+    def get_interests():
+
+        return ['Interest1', "Interest2", "Interest3"]
 
     try:
-        ALL_LAYERS = {
-            "Bike Rentals": pdk.Layer(
-                "HexagonLayer",
-                data=from_data_file("bike_rental_stats.json"),
-                get_position=["lon", "lat"],
-                radius=200,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
-                extruded=True,
-            ),
-            "Bart Stop Exits": pdk.Layer(
-                "ScatterplotLayer",
-                data=from_data_file("bart_stop_stats.json"),
-                get_position=["lon", "lat"],
-                get_color=[200, 30, 0, 160],
-                get_radius="[exits]",
-                radius_scale=0.05,
-            ),
-            "Bart Stop Names": pdk.Layer(
-                "TextLayer",
-                data=from_data_file("bart_stop_stats.json"),
-                get_position=["lon", "lat"],
-                get_text="name",
-                get_color=[0, 0, 0, 200],
-                get_size=15,
-                get_alignment_baseline="'bottom'",
-            ),
-            "Outbound Flow": pdk.Layer(
-                "ArcLayer",
-                data=from_data_file("bart_path_stats.json"),
-                get_source_position=["lon", "lat"],
-                get_target_position=["lon2", "lat2"],
-                get_source_color=[200, 30, 0, 160],
-                get_target_color=[200, 30, 0, 160],
-                auto_highlight=True,
-                width_scale=0.0001,
-                get_width="outbound",
-                width_min_pixels=3,
-                width_max_pixels=30,
-            ),
-        }
-        st.sidebar.markdown("### Map Layers")
-        selected_layers = [
-            layer
-            for layer_name, layer in ALL_LAYERS.items()
-            if st.sidebar.checkbox(layer_name, True)
-        ]
-        if selected_layers:
-            st.pydeck_chart(
-                pdk.Deck(
-                    map_style="mapbox://styles/mapbox/light-v9",
-                    initial_view_state={
-                        "latitude": 37.76,
-                        "longitude": -122.4,
-                        "zoom": 11,
-                        "pitch": 50,
-                    },
-                    layers=selected_layers,
-                )
-            )
+        interests = get_interests()
+        selected = st.multiselect(
+            "Choose interests 👇️", interests, [
+                "Interest1"]
+        )
+        if not selected:
+            st.error("Please select at least one interest.")
         else:
-            st.error("Please choose at least one layer above.")
+            st.write("Running recommendation model with interests:")
+            for item in selected:
+                st.write(item)
+
+            if st.button("Get Results", type='primary'):
+                run_model(selected)
+
     except URLError as e:
         st.error(
             """
-            **This demo requires internet access.**
-            Connection error: %s
+            **Something went wrong: %s
         """
             % e.reason
         )
@@ -97,11 +47,7 @@ st.set_page_config(page_title="Recommendation", page_icon="📊")
 st.markdown("# Recommendation")
 st.sidebar.header("Recommendation")
 st.write(
-    """This demo shows how to use
-[`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
-to display geospatial data."""
+    """Choose below what you are interested in when looking for a place to live and see what recommendations you will get based on that"""
 )
 
-mapping_demo()
-
-# show_code(mapping_demo)
+recommendation()
